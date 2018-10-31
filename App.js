cec595cc80f301f87e6d015433e3e0e23befeb55
Roman Dinkel <dinkelroman@gmail.com>
2018-10-31 13:40:28 +0100

    - FIXED: cannot compile via gradlew because of an Android dependency issue - put camera component from lib in its own component - added scanning barcode and showing transaction message as human readable text - you can take a picture with the camera. file is cached and not in users photo library

--- App.js
+++ App.js
@@ -7,33 +7,51 @@
  */
 
 import React, {Component} from 'react';
-import {Alert, Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {Alert, Platform, StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 import {hex2ascii} from 'hex2ascii';
-import {RNCamera, FaceDetector} from 'react-native-camera';
+import {RNCamera} from 'react-native-camera';
 
 const GREETING_PLATFORM_SPECIFIC_USER = Platform.select({
-    ios: 'Welcome Apple User!',
+    ios: 'Hello Apple User!',
     android:
         'Hello Android User!\n' +
-        'This is not the droid we are looking for !!',
+        'Locate QRCode with the Camera!',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.handleBarcode = this.handleBarcode.bind(this);
+        this.state = {showCamera: false};
+    }
+
+    async handleBarcode(barcode) {
+        console.log(barcode);
+        this.toggleShowCamera();
+        let hexMessage = await getMessageFromGivenETHTransactionByHash(barcode);
+        Alert.alert(hex2ascii(hexMessage));
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>{GREETING_PLATFORM_SPECIFIC_USER}</Text>
                 <Button
                     onPress={async () => {
-                        let ethHashFromQRCode = '0x64eed727b690a2a0aa1f9519c5ac68919a0a3fa6cbd2512be41fd536a9054f52';
-                        let hexMessage = await getMessageFromGivenETHTransactionByHash(ethHashFromQRCode);
-                        Alert.alert(hex2ascii(hexMessage));
+                        this.toggleShowCamera();
                     }}
-                    title="Press Me"
+                    title="Activate Camera for scanning an item"
                 />
+                {this.state.showCamera ? <Camera onBarcodeDetection={this.handleBarcode}/> : null}
             </View>
         );
+    }
+
+    toggleShowCamera() {
+        this.setState(previousState => {
+            return {showCamera: !previousState.showCamera};
+        });
     }
 }
 
@@ -51,6 +69,52 @@ async function getMessageFromGivenETHTransactionByHash(hash) {
     }
 }
 
+export class Camera extends Component {
+    constructor(props) {
+        super(props);
+        this.handleBarcodeDetection = this.handleBarcodeDetection.bind(this);
+    }
+
+    handleBarcodeDetection(e) {
+        // console.log(e.barcodes[0].data);
+        this.props.onBarcodeDetection(e.barcodes[0].data);
+    }
+
+    render() {
+        return (
+            <View style={styles.containerCamera}>
+                <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    permissionDialogTitle={'Permission to use camera'}
+                    permissionDialogMessage={'We need your permission to use your camera phone'}
+                    onGoogleVisionBarcodesDetected={this.handleBarcodeDetection}
+                />
+                <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+                    <TouchableOpacity
+                        onPress={this.takePicture.bind(this)}
+                        style={styles.capture}
+                    >
+                        <Text style={{fontSize: 14}}> SNAP </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+
+    takePicture = async function () {
+        if (this.camera) {
+            const options = {quality: 0.5, base64: true};
+            const data = await this.camera.takePictureAsync(options);
+            console.log(data.uri);
+        }
+    };
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -65,4 +129,23 @@ const styles = StyleSheet.create({
         color: '#d5d5d5',
         margin: 10,
     },
+    containerCamera: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black'
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20
+    }
 });
